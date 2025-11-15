931df50d6b2ac52b437461cc0012c18388876350
titu2razvan <razvanandronie2005@gmail.com>
2025-11-15 17:00:17 +0200

    Delete frontend directory

--- frontend/components/icons/MapIcon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const MapIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.5-12.75 4.5 1.5M15 5.25 4.5 9m15 0-4.5-1.5m0 0-4.5 1.5M4.5 9l-2.25 2.25M19.5 12l2.25-2.25M4.5 9v11.25m15-11.25V20.25M4.5 20.25 9 17.25m10.5 3-4.5-3m0 0-4.5 3m4.5-3v-3" />
-  </svg>
-);
