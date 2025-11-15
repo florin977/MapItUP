9e57f7f9261a5945a23a86b4b44cda752d18e5c9
titu2razvan <razvanandronie2005@gmail.com>
2025-11-15 03:09:40 +0200

    initial

---
+++ src/pages/AdminPage.jsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+  return (
+    <div className="page-container">
+      <h1>Admin Dashboard</h1>
+      <p>Pagina de admin (momentan goală).</p>
+    </div>
+  );
+}
